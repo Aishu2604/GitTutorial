bc4153e79f24d616e179c376667057a80bbd6e4d
Aishu2604 <aishwary.nigam04@gmail.com>
2022-08-17 00:39:24 +0530

    Task5

--- TASK3.html/dom.js
+++ TASK3.html/dom.js
@@ -21,9 +21,15 @@
 //var headerClass = document.getElementById('main');
 //headerClass.innerHTML = <h2 style="color: green;"> ADD ITEMS</h2>
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-items[2].style.backgroundColor = 'green';
-for(var i = 0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//items[2].style.backgroundColor = 'green';
+//for(var i = 0;i<items.length;i++){
+//    items[i].style.fontWeight = 'bold';
+//}
+var li = document.getElementsByTagName('li');
+console.log(li);
+li[2].style.backgroundColor = 'green';
+for(var i = 0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold';
 }
