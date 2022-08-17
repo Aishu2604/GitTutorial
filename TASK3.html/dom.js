d6d5b838e62ff0ab45f82cdaec5c4792b125b106
Aishu2604 <aishwary.nigam04@gmail.com>
2022-08-17 16:08:43 +0530

    Task7

--- TASK3.html/dom.js
+++ TASK3.html/dom.js
@@ -43,7 +43,83 @@
 //var items = document.querySelectorAll('li:nth-child(2)');
 //items.style.color = 'green';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = 'green';
-}
+//var odd = document.querySelectorAll('li:nth-child(odd)');
+//for(var i=0;i<odd.length;i++){
+//    odd[i].style.backgroundColor = 'green';
+//}
+
+var itemList = document.querySelector('#items');
+//parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+console.log(itemList.childNodes);
+
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
+
+//firstChild
+console.log(itemList.firstChild);
+//firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello bro';
+
+//lastChild
+console.log(itemList.lastChild);
+//lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello Beta';
+
+//nextSibling
+console.log(itemList.nextSibling);
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+
+//previousSibling
+console.log(itemList.previousSibling);
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green';
+
+//createElement
+
+//create a div
+var newDiv = document.createElement('div');
+
+//add class
+newDiv.className = 'Hello chote';
+
+//add attr
+newDiv.setAttribute('title','Hello div');
+
+//create text node
+var newDivText = document.createTextNode('Hello')
+
+//Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv,h1);
+
+var item = document.querySelector('ul .title');
+var h2 = document.querySelector('h2 ul');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+item.insertBefore(newDiv,h3);
